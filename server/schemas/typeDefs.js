4acfc6bbe9965ec0558e92ff1c2545325bf1caeb
Mona Lisa <joannagrosshans2@gmail.com>
2021-02-02 14:29:23 -0600

    typedefs updated

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Query {
+        me: [User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(save: SaveBookInput): User
+        removeBook(bookId: INT!): User
+    }
+
+    input SaveBookInput {
+        book {
+            author
+            description
+            title
+            bookId
+            image
+            link
+        }
+    }
+
+    type User {
+        _id
+        username
+        email
+        bookCount
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+
+    type Auth {
+        token
+        user: [User]
+    }
+`;
+
+module.exports = typeDefs
